daf5ff9a50bc938fea598c6bc4a928a730fbf2f5
swamp1820 <swamp1820@yandex.ru>
2020-11-27 18:48:34 +0300

    add api and models

---
+++ LodFinals.Api/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LodFinals.Api.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Exercise> Exercises { get; set; }
+    }
+}
