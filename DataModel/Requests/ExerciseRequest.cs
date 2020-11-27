6f88df640b186abe40488a571bf44e3ffa1235fa
swamp1820 <swamp1820@yandex.ru>
2020-11-28 01:31:22 +0300

    добавил реквесты для удобного заполнения БД

---
+++ DataModel/Requests/ExerciseRequest.cs
@@ -0,0 +1,24 @@
+﻿using DataModel.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel.Requests
+{
+    public class ExerciseRequest
+    {
+        public ExerciseType ExerciseType { get; set; }
+
+        public int Score { get; set; }
+
+        public string Description { get; set; }
+
+        public byte[] Data { get; set; }
+
+        public string Keywords { get; set; }
+
+        public uint Order { get; set; }
+
+        public int SyllabusId { get; set; }
+    }
+}
