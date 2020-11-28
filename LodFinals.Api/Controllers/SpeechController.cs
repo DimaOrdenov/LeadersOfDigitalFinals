c3735190117c239ef8672493a404a79dff40feb0
swamp1820 <swamp1820@yandex.ru>
2020-11-28 17:02:53 +0300

    add tts

---
+++ LodFinals.Api/Controllers/SpeechController.cs
@@ -0,0 +1,29 @@
+﻿using LodFinals.Api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LodFinals.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpeechController : ControllerBase
+    {
+        private readonly SpeechService _speechService;
+
+        public SpeechController(SpeechService speechService)
+        {
+            _speechService = speechService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> TextToSpeech(string text,string language)
+        {
+            string result = await _speechService.TextToSpeech(text, language);
+            return Ok(result);
+        }
+    }
+}
