2870f2dc10a3e8a101f7bb6df31681ea1dcc309d
Колчин Сергей Алексеевич <sakolch3@mts.ru>
2020-11-28 14:04:56 +0300

    add lex service

---
+++ LodFinals/Services/ILexService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LodFinals.Services
+{
+    public interface ILexService
+    {
+        Task<string> Conversation(string phrase);
+        Task<string> GetIntent(string phrase);
+        void SetUser(string userId);
+    }
+}
---
+++ LodFinals/Services/LexService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.Lex;
+using Amazon.Lex.Model;
+
+namespace LodFinals.Services
+{
+    public class LexService : ILexService
+    {
+        private readonly AmazonLexClient _client;
+        private string _userId;
+
+        public LexService(AmazonLexClient amazonLexClient)
+        {
+            _client = amazonLexClient;
+        }
+
+        public void SetUser(string userId)
+        {
+            _userId = userId;
+        }
+
+        public async Task<string> GetIntent(string phrase)
+        {
+            var r = new PostTextRequest() { BotName = "LodFinals", UserId = _userId, InputText = phrase, BotAlias = "lod_intent" };
+            return (await Send(r)).IntentName;
+        }
+
+        public async Task<string> Conversation(string phrase)
+        {
+            var r = new PostTextRequest() { BotName = "LodConversation", UserId = _userId, InputText = phrase, BotAlias = "lod_conversation" };
+            return (await Send(r)).IntentName;
+        }
+
+        private async Task<PostTextResponse> Send(PostTextRequest request) =>
+            await _client.PostTextAsync(request);
+    }
+}
